8184bede9cd0277e074001e847a5021781241589
Nardelys <nardelys15c@gmail.com>
2023-07-29 19:15:27 -0400

    started Java api's

--- src/app.js
+++ src/app.js
@@ -1,1 +1,4 @@
+let apiKey = "5c97bd60362f1c74e3f19cd744b9a085  ";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=imperial`;
 
+axios.get(Url).then(displaytemperature);
