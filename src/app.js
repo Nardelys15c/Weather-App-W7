6523f705a9b8e323d69603ce652b0546abf2ef84
Nardelys <nardelys15c@gmail.com>
2023-07-30 15:30:55 -0400

    updated ccs console.log temp

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
-let apiKey = "5c97bd60362f1c74e3f19cd744b9a085  ";
+function displayTemperature(response) {
+  console.log(response.data.main.temp);
+}
+
+let apiKey = "5c97bd60362f1c74e3f19cd744b9a085";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=imperial`;
 
-axios.get(Url).then(displaytemperature);
+axios.get(apiUrl).then(displayTemperature);
