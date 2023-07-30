cedc339a35db1409250877d6c278da1542d990ca
Nardelys <nardelys15c@gmail.com>
2023-07-30 16:00:17 -0400

    updated JS city, temp & description

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 function displayTemperature(response) {
-  console.log(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let description = document.querySelector("#description");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  description.innerHTML = response.data.weather[0].description;
 }
 
 let apiKey = "5c97bd60362f1c74e3f19cd744b9a085";
